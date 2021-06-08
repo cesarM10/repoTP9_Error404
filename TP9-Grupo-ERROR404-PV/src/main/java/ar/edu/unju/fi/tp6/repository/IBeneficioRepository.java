09089f22a31cd828b696308427c20590581fcb2e
Windows <Windows@IVAN>
2021-06-08 18:29:00 -0300

    responsable:Salas, Ivan Arnaldo. Se ha creado IbeneficioRepository y se agregaron un icono en footer.

---
+++ TP9-Grupo-ERROR404-PV/src/main/java/ar/edu/unju/fi/tp6/repository/IBeneficioRepository.java
@@ -0,0 +1,9 @@
+package ar.edu.unju.fi.tp6.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import ar.edu.unju.fi.tp6.model.Beneficio;
+
+public interface IBeneficioRepository extends CrudRepository<Beneficio , Integer>{
+	         public Beneficio findById(Long id);
+}
